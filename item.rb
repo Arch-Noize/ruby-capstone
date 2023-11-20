991add77eaf4d7690af0fb51c09392c5bb5e754e
Arch-Noize <santiagonoize@gmail.com>
2023-11-20 10:04:39 -0400

    Add item.rb and method

---
+++ item.rb
@@ -0,0 +1,23 @@
+class Item
+  attr_accessor :id, :author, :source, :label, :publish_date, :archived
+
+  def initialize(publish_date)
+    @id = Random.rand(1..1000)
+    @genre = nil
+    @author = nil
+    @source = nil
+    @label = nil
+    @publish_date = publish_date
+    @archived = false
+  end
+
+  def can_be_archived?
+    # Time.now.year is an internal ruby class that prints out the current year
+    (Time.now.year - @publish_date.year) > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived? == true
+    nil
+  end
+end
