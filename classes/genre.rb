f939074b760a001bd794f892c554099c697cc9be
Arch-Noize <santiagonoize@gmail.com>
2023-11-21 09:43:02 -0400

    Fix genre class and tests

--- classes/genre.rb
+++ classes/genre.rb
@@ -5,13 +5,13 @@ class Genre
   attr_accessor :name
 
   def initialize(name)
-    @id = Random.rand(1...1000)
+    @id = Random.rand(1..1000)
     @name = name
     @items = []
   end
 
   def add_item(item)
-    @items << self
-    item.add_item = self
+    @items << item
+    item.genre = self
   end
 end
--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -2,15 +2,17 @@ require 'rspec'
 require './classes/genre'
 
 describe Genre do
-  let(:item) { double('Item') }
+    let(:name) { 'Rock' }
+    let(:item) { double('Item') }
+    subject(:genre) { Genre.new(name) }
 
-  before :each do
-    name = 'Rock'
-    @genre = Genre.new(name)
-  end
-
-  it 'adds item inside the genre' do
-    @genre.add_item(item)
-    expect(@genre.items).to include(item)
-  end
+    it 'The name of the genre should be Rock' do
+        expect(genre.name).to eql(name)
+    end
+  
+    it 'add_item should add a new item into the genre' do
+      expect(item).to receive(:genre=).with(genre)
+      genre.add_item(item)
+      expect(genre.items).to include(item)
+    end
 end
